18a0304005b76fa750d26ec5d09c85318e6a2a5a
velzepooz <velzepooz@gmail.com>
2021-03-06 15:21:17 +0200

    cli test v3

---
+++ backend/www/precache-manifest.55ece82ec99c7b996f65bd934d45a8c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1474fa5d003d01c77f6e620a00074c51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6eef09448232ba046160",
+    "url": "./static/css/main.ac571798.chunk.css"
+  },
+  {
+    "revision": "61dd500cc28b2b51ce44",
+    "url": "./static/js/2.bb54d105.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "./static/js/2.bb54d105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eef09448232ba046160",
+    "url": "./static/js/main.93de6ea9.chunk.js"
+  },
+  {
+    "revision": "a1209cc57316d425a15b",
+    "url": "./static/js/runtime-main.f74aba74.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/media/Heebo-Bold.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/media/Heebo-Medium.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "./static/media/Heebo-Regular.d41d8cd9.ttf"
+  }
+]);
